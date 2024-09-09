f2d9736e1ca7541357782b183b3c9319672eb6b1
Naelson Pedro <119671481+NaelsonPedro@users.noreply.github.com>
2024-09-09 19:21:58 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+
+let dados = [
+{
+	titulo: "Rayssa Leal",
+	descricao: "Raissa Leal é uma skatista brasileira, vice-campeã olímpica e um dos maiores talentos do skate mundial.",
+	link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+	tags: "skate fada fadinha"
+},
+{
+	titulo: "Rebeca Andrade",
+	descricao: "Rebeca Andrade é uma das maiores atletas de ginástica do Brasil, ganhando o primeiro ouro na modalidade e conquistando o coração dos Brasileiros.",
+	link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+	tags: "ginastica ouro ginasta"
+},
+{
+	titulo: "Beatriz Souza",
+	descricao: "Beatriz Souza é uma judoca brasileira, conhecida por sua técnica e força.",
+	link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+	tags: "judo judô judoka"
+}
+]
+
